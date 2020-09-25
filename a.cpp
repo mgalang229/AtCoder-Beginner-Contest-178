3359aad51dc6f40cc9e6edee1fda30856e7f2388
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-09-25 19:50:47 +0800

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int x;
+	cin >> x;
+	if (x == 1) {
+		cout << 0;
+	} else {
+		cout << 1;
+	}
+	cout << '\n';
+	return 0;
+}
